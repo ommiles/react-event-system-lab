bbeeb99fbcd8f1406e2c032da7803fcdf6aff2c0
OM Miles <olivia.m.miles@gmail.com>
2021-05-02 16:18:13 -0700

    lab complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+export default class EyesOnMe extends Component {
+
+    handleFocus = () => {
+        console.log('Good!')
+    }
+    
+    handleBlur = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    render() {
+        return (
+            <div>
+                <button
+                onFocus={this.handleFocus}
+                onBlur={this.handleBlur}
+                >Submit</button>
+            </div>
+        )
+    }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
-// Code Keypad Component Here
+import React, { Component } from 'react'
+
+export class Keypad extends Component {
+
+    handleKeyUp = () => {
+        console.log('Entering password...')
+    }
+
+    render() {
+        return (
+            <div>
+                <input 
+                type="password"
+                onKeyUp={this.handleKeyUp} />
+            </div>
+        )
+    }
+}
+
+export default Keypad
